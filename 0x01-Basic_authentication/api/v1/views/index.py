2a5c96211ce0f5e4b9890a61a89a24fd3d5b855e
Mark Allen Gbalazeh <33788083+Markcino@users.noreply.github.com>
2024-07-12 10:51:18 +0000

    update

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -2,6 +2,7 @@
 """ Module of Index views
 """
 from flask import jsonify, abort
+from flask import abort
 from api.v1.views import app_views
 
 
@@ -24,3 +25,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - 401
+    """
+    return abort(401)
